fe781548e4384744f44ab714b30ab0a31998edde
Laura de Oliveira <l4ur4.88@gmail.com>
2024-07-11 20:45:02 -0300

    Lesson 1 - Creating Trip files

---
+++ src/main/java/com/nlw/planner/trip/TripController.java
@@ -0,0 +1,38 @@
+package com.nlw.planner.trip;
+
+import com.nlw.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository repository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
+        Trip newTrip = new Trip(payload);
+
+        this.participantService.registerParticipantsToTrip(payload.emails_to_invite(), newTrip.getId());
+
+        this.repository.save(newTrip);
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
