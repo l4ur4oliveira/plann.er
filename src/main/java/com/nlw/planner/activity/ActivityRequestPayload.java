2b1013affaf12e828e780572f7e2656f9b8abe95
Laura de Oliveira <l4ur4.88@gmail.com>
2024-07-13 20:47:02 -0300

    Lesson 3 - Removing unused dependencies

--- src/main/java/com/nlw/planner/activity/ActivityRequestPayload.java
+++ src/main/java/com/nlw/planner/activity/ActivityRequestPayload.java
@@ -1,8 +1,4 @@
 package com.nlw.planner.activity;
 
-import com.nlw.planner.trip.Trip;
-
-import java.time.LocalDateTime;
-
 public record ActivityRequestPayload(String title, String occurs_at) {
 }
